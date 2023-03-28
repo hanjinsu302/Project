2dfd36f21cfd5a073262d4b9a24e33b9cfcb019e
Hanjinsu302 <jinsuhan302@gmail.com>
2023-03-28 19:57:03 +0900

    get, post form 전송

--- 08_form/app.js
+++ 08_form/app.js
@@ -4,12 +4,26 @@ const PORT = 8000;
 
 app.set("view engine", "ejs"); //view engine 등록
 app.use("/views", express.static(__dirname + "/views")); // ejs를 담을 views 폴더 경로
+app.use(express.urlencoded({ extended: true })); //post 요청으로 들어오는 모든 형식의 데이터를 파싱
+app.use(express.json()); //json 형태로 데이터를 주고 받음
 
 // 라우팅: 경로 설정
-//브라우저에서 어던 url로 접속했을때 어떤 페이지를 보여줄 것 인가?
+//브라우저에서 어떤 url로 접속했을때 어떤 페이지를 보여줄 것 인가?
 
 app.get("/", function (req, res) {
-  res.render("index"); // views/index.ejs 파일을 찾아서 응답
+  const myTitle = "폼 실습을 합시다~~~";
+  res.render("index", { title: myTitle }); // views/index.ejs 파일을 찾아서 응답
+});
+
+app.get("/getForm", function (req, res) {
+  //console.log(req);
+  console.log(req.query); //사용자가 브라우저에서 입력한 값
+  res.send("get요청성공");
+});
+
+app.post("/postForm", function (req, res) {
+  console.log(req.body);
+  res.send("post요청 성공");
 });
 
 app.listen(PORT, function () {
