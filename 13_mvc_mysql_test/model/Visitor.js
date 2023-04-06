a988a386573fc60df600586573a50d88ca72034a
Hanjinsu302 <jinsuhan302@gmail.com>
2023-04-06 19:55:13 +0900

    mvc_ test

--- 13_mvc_mysql_test/model/Visitor.js
+++ 13_mvc_mysql_test/model/Visitor.js
@@ -11,7 +11,7 @@ const mysql = require("mysql");
 
 // database 연결
 const conn = mysql.createConnection({
-  host: "127.0.0.1", // databse 가 설치된 ip 주소
+  host: "localhost", // databse 가 설치된 ip 주소
   user: "user", // 데이터베이스 접속 계정명
   password: "1234", // 데이터베이스 접속 비번
   database: "condingon", // 사용할 데이터베이스 이름
@@ -22,13 +22,13 @@ exports.getVisitors = (callback) => {
   // -> conn 에 저장되어 있는 데이터베이스 접근해서 sql문 실행
   // -> 결과를 callback 함수에 넘겨줌
   const sql = "SELECT * FROM visitor;";
-
   conn.query(sql, (err, rows) => {
     if (err) {
       throw err;
     }
 
     console.log("Visitor.js >> ", rows);
+    // => [ {}, {}, {} ]
     callback(rows);
   });
 };
@@ -58,5 +58,33 @@ exports.deleteVisitor = (id, callback) => {
 
     console.log("Visitor.js >> ", rows);
     callback(true);
+  });
+};
+
+exports.getVisitor = (id, callback) => {
+  console.log(id); // controller에서 보내주는 req.query.id
+
+  const sql = `select * from visitor where id=${id};`;
+  conn.query(sql, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Visitor.js >>", rows);
+    callback(rows[0]);
+  });
+};
+
+exports.patchVisitor = (data, callback) => {
+  console.log(data); // controller에서 넘겨주는 req.body
+
+  const sql = `update visitor set name='${data.name}', comment='${data.comment}' WHERE id=${data.id}`;
+  conn.query(sql, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Visitor.js >>", rows);
+    callback();
   });
 };
